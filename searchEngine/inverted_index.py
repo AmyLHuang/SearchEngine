305f1a6cd74faad1040092616b05f64e143195cd
Amy Huang <goten02028@gmail.com>
2023-06-21 15:45:14 -0700

    implement inverted index

---
+++ searchEngine/inverted_index.py
@@ -0,0 +1,11 @@
+import os
+import json
+from bs4 import BeautifulSoup
+
+
+class InvertedIndex:
+    def __init__(self, docs_dir):
+        self.docs_dir = docs_dir
+
+    def build_index(self):
+        pass
---
+++ searchEngine/main.py
@@ -0,0 +1,17 @@
+from nltk.stem import PorterStemmer
+from inverted_index import InvertedIndex
+import os.path
+
+
+def initialize_index():
+    indexExists = os.path.exists("index")
+    if indexExists == False:
+        ii = InvertedIndex("../documents/analyst/ANALYST")
+        ii.build_index()
+
+
+if __name__ == "__main__":
+    # if inverted index does not exist, build it
+    initialize_index()
+
+    # queries
